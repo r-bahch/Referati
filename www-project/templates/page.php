8f8948a1be2fc4c814e2131ee655d42c8fcd344f
Radoslav Bahchevanov <r.bahchevanov@gmail.com>
2016-04-04 15:35:42 +0300

    Upload files

---
+++ www-project/templates/page.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Referati</title>
+<link rel="stylesheet" href="css/style.css" type="text/css"/>
+</head>
+
+<body>
+<div id="header">
+    <div id="left">
+        <label>Система за работа с реферати</label>
+    </div>
+    <div id="right">
+        <div id="content">
+            Влезли сте като: <?php echo $userRow['username']; ?>&nbsp;<a href="logout.php?logout">Изход</a>
+        </div>
+    </div>
+</div>
+<div id='cssmenu' align="center">
+    <ul>
+        <li><a href="home.php"><span>Начало</span></a></li>
+        <li><a href="subject-select.php"><span>Избор на тема</span></a></li>
+        <li class='last'><a href='submit-file.php'><span>Предаване</span></a></li>
+        <li><a href="write-review.php"><span>Рецензия</span></a></li>
+        <li><a href="my-reviews.php"><span>Моите рецензии</span></a></li>
+    </ul>
+</div>
